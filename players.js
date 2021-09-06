14fd9a7c8af025ad88e3bad49828928fb0cd2471
delzhand <delzhand@gmail.com>
2021-09-06 19:24:27 -0400

    update

---
+++ players.js
@@ -0,0 +1,12 @@
+const players = {
+  'U-wetatso': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Tsa-huro': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Mosadatsi': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Disintu': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Sha-ea': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Error: Divide By Zero': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'An Exception Has Occurred': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Landing Gear Damaged': {stability: 2, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Could Not Locate "Name"': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+  'Core Dump': {stability: 3, accuracy: 3, speed: 3, skill: 3, talent: 'none'},
+};
