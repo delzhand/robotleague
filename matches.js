70bfe176b95f7bcb7d81e040d00a10b730f9f5d7
delzhand <delzhand@gmail.com>
2021-09-06 21:04:00 -0400

    update

---
+++ matches.js
@@ -0,0 +1,36 @@
+const matches = [
+  {
+    randomSeed: 'test',
+    fieldSize: 12,
+    totalRounds: 5,
+    balls: 3,
+    bigBalls: 1,
+    home: {
+      name: 'Wavelengths',
+      players: ['U-wetatso', 'Tsa-huro', 'Mosadatsi'],
+      bench: ['Disintu', 'Sha-ea']
+    },
+    away: {
+      name: 'Irregulars',
+      players: ['Error: Divide By Zero', 'An Exception Has Occurred', 'Landing Gear Damaged'],
+      bench: ['Could Not Locate Name', 'Core Dump']
+    }
+  },
+  {
+    randomSeed: 'test',
+    fieldSize: 12,
+    totalRounds: 5,
+    balls: 3,
+    bigBalls: 1,
+    home: {
+      name: 'Wayfarers',
+      players: ['Glasgow Hammer', 'Calamity Rescue', 'Leuchtturm Hans'],
+      bench: ['Hazard Kraken', 'Duster Catapult']
+    },
+    away: {
+      name: 'Rovers',
+      players: ['Legion Red', 'Vicious Whisper', 'King Bull'],
+      bench: ['Ultimate Gun-Head', 'Astro Nergal']
+    }
+  },
+];
