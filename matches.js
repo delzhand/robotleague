5b270d422a20b337dc3e9d899d4f5d6c0af2bbd5
delzhand <delzhand@gmail.com>
2021-09-08 15:59:54 -0400

    preseason

--- matches.js
+++ matches.js
@@ -6,23 +6,6 @@ const matches = [
     balls: 3,
     bigBalls: 1,
     home: {
-      name: 'Wavelengths',
-      players: ['U-wetatso', 'Tsa-huro', 'Mosadatsi'],
-      bench: ['Disintu', 'Sha-ea']
-    },
-    away: {
-      name: 'Irregulars',
-      players: ['Error: Divide By Zero', 'An Exception Has Occurred', 'Landing Gear Damaged'],
-      bench: ['Could Not Locate Name', 'Core Dump']
-    }
-  },
-  {
-    randomSeed: 'test',
-    fieldSize: 12,
-    totalRounds: 5,
-    balls: 3,
-    bigBalls: 1,
-    home: {
       name: 'Wayfarers',
       players: ['Glasgow Hammer', 'Calamity Rescue', 'Leuchtturm Hans'],
       bench: ['Hazard Kraken', 'Duster Catapult']
@@ -40,14 +23,31 @@ const matches = [
     balls: 3,
     bigBalls: 1,
     home: {
-      name: 'Magnets',
+      name: 'Rovers',
+      players: ['Legion Red', 'Vicious Whisper', 'King Bull'],
+      bench: ['Ultimate Gun-Head', 'Astro Nergal']
+    },
+    away: {
+      name: 'Lovers',
+      players: ['Guardian Preserver', 'Electro Blossom', 'Beautiful Amavasya'],
+      bench: ['Shadow Virago', 'Lotus of Delight']
+    }
+  },
+  {
+    randomSeed: 'test',
+    fieldSize: 12,
+    totalRounds: 5,
+    balls: 3,
+    bigBalls: 1,
+    home: {
+      name: 'Lovers',
+      players: ['Guardian Preserver', 'Electro Blossom', 'Beautiful Amavasya'],
+      bench: ['Shadow Virago', 'Lotus of Delight']
+    },
+    away: {
+      name: 'Wayfarers',
       players: ['Saxophone Blue', 'Melodious Mistake', 'Queen\'s Gambit'],
       bench: ['Radiant Memento', 'Weeping Willow']
     },
-    away: {
-      name: 'Lasers',
-      players: ['Self Entitled', 'Listen Here You', 'Cry Power'],
-      bench: ['Beyond the Hour', 'Lacking in Culture if Not Grace']
-    }
-  },
+  }
 ];
